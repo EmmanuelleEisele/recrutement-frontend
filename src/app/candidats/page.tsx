1c141a7a8946136ec55590c9697438e5ed9f2a4d
Emmanuelle <emmanuelle.eisele@oclock.school>
2025-04-10 10:19:26 +0200

    removing some tests in Layout

--- src/app/candidats/page.tsx
+++ src/app/candidats/page.tsx
@@ -1,8 +1,23 @@
 
+interface ICandidats{
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
 export default async function CandidatsPage() {
+  const res= await fetch('https://reqres.in/api/users');
+  const response = await res.json();
+  console.log(response)
     return(
     <div>
-    <h1>Liste des candidats</h1>
+    <h1 style={{textAlign:'center'}}>Liste des candidats</h1>
+
+    {response.data.map((d: ICandidats)=>{
+      return <p key={d.id} style={{fontWeight:'bold', textAlign:'center'}}>{d.first_name}</p>
+      })}
     </div>
 
     )
