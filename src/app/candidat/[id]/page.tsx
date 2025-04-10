02ae4606b0603bae1dc980134f90e2f104baf9a8
Emmanuelle <emmanuelle.eisele@oclock.school>
2025-04-10 16:13:29 +0200

    dynamization candidat detail

--- src/app/candidat/[id]/page.tsx
+++ src/app/candidat/[id]/page.tsx
@@ -1,3 +1,39 @@
-export default function CandidatPage({params}:{params:{id:number}}) {
-    return <h1>Detail du candidat N°{params.id}</h1>;
+'use server'
+import { candidats } from "@/app/candidatsList"
+import { Button, Card, List } from "antd";
+import './page.css';
+
+interface ICandidats {
+  id: number;
+  name: string;
+  email: string;
+  Github: string;
+  experience: string;
+  motivation: string;
+}
+
+export default async function CandidatPage({ params }: { params: { id: number } }) {
+  const candidat = candidats.find((c) =>  c.id === Number(params.id) );
+  console.log(candidat)
+
+  if (!candidat) {
+    return <h1>Candidat non trouvé</h1>;
   }
+
+  return (
+    <>
+      <h1 className="title-candidat">Fiche du candidat N°{candidat.id}</h1>
+
+      <Card className="card" title={candidat.name} variant="borderless" >
+        <h3 className="card-title">Nom Github</h3>
+        <p className="card-description">{candidat.github}</p>
+        <h3 className="card-title">Experiences</h3>
+        <p className="card-description">{candidat.experience}</p>
+        <h3 className="card-title">Motivations</h3>
+        <p className="card-description">{candidat.motivation}</p>
+      
+      </Card>
+      <Button href='/' style={{position: 'absolute', top: '0.5rem', left:'0.5rem'}}>Accueil</Button>
+    </>
+  );
+}
